f1dd214cf5cd4f9b3a4dec405b0499fdd7a8ed64
IsseiClockWise <112615226+IsseiClockWise@users.noreply.github.com>
2023-06-29 18:17:36 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TodoList from './TodoList.js';
+
+function App() {
+  return (
+    <div>
+      <div className="App">ToDoアプリ（試作品）</div>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,11 @@
+//rafce を入力しtab変換すると以下か表示される
+
+//import React from 'react'; これはなくてもOK![](https://storage.googleapis.com/zenn-user-upload/358cf26befcb-20230118.png)
+
+const TodoList = () => {
+  return (
+    <div>TodoListコンポーネント</div>
+  )
+}
+
+export default TodoList
